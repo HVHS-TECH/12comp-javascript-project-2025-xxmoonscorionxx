2c0949c6ac771aadf0e841349e326d0a3e35a3f3
xxmoonscorionxx <22091wk@hvhs.school.nz>
2025-04-10 22:59:56 +0000

    refining2

--- js_script_container/script.js
+++ js_script_container/script.js
@@ -12,6 +12,7 @@
 let gameState = "intro";
 let startImage;
 let startText;
+let spaceshipModel;
 let asteroidModel;
 let coinModel;
 let clickedCounter = 0;
@@ -32,6 +33,7 @@ let particlesSpawningAmount;
 let praticlesSpawningPart;
 let playtext1;
 let playtext2;
+
 /*******************************************************/
 //const container
 /*******************************************************/
@@ -44,25 +46,25 @@ const COINSPAWNINGCHANCEPARAMETER = 2;
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-	cnv = new Canvas(windowWidth-4 , windowHeight-4);
+	cnv = new Canvas(windowWidth - 4, windowHeight - 4);
 	asteroidGroup = new Group();
 	coinGroup = new Group();
-	if(gameState == "intro") {
-			background('#0e001b');
-			startImage = new Sprite(900, 400, 200, 400, 's');
-			startImage.image = startText; 
-		}
-	else if(gameState == "play") {
-			startImage = null;
-			
-			
-			
-			
-		}
-	else if(gameState == "lose") {	
- }
+	if (gameState == "intro") {
+		background('#0e001b');
+		startImage = new Sprite(900, 400, 200, 400, 's');
+		startImage.image = startText;
+	}
+	else if (gameState == "play") {
+		startImage = null;
+
+
+
+
+	}
+	else if (gameState == "lose") {
+	}
 }
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
@@ -73,80 +75,61 @@ function draw() {
 // functions()
 /*******************************************************/
 function playState() {
-	if(gameState == "intro") {
+	if (gameState == "intro") {
 		background('#0e001b');
-		if(clickedCounter == 1) {
+		if (clickedCounter == 1) {
 			startImage.remove();
 			gameState = "play"
 		}
 	}
-	else if(gameState == "play") {
-		
+	else if (gameState == "play") {
+
 		background('#0e001b');
+
 		//* Coding for the spaceship*//
 		info();
-		if(spaceshipSpawned == false) {
-			spaceship  = new Sprite(200, 400, 100, 40, 'd');
-			spaceshipSpawned = true;
-			console.log("ITS HAPPENING")
-		}
-		spaceship.rotationSpeed = 0;
-		spaceship.rotation = 0;
-		asteroidGroup.rotationSpeed = 0;
-		asteroidGroup.rotation = 0;
-		spaceshipMovement();
+
 
 		//** Coding for Bodrers **/
-		if(borderSpawned == false) {
+		if (borderSpawned == false) {
 			borders();
 			borderSpawned = true
 		}
+		spaceshipCodeContainer();
+		asteroidCodeContainer();
 
-		//* Coding for Asteroid*//
-		asteroidSpawningChance = random(1,100);
-		asteroidSpawingLocationY = random(1,windowHeight);
-		if(asteroidSpawned == true) {
-			asteroidMovement();
-		}
-		if(ASTEROIDSPAWNINGCHANCEPARAMETER>asteroidSpawningChance) {
-			asteroid = new Sprite(windowWidth+50,asteroidSpawingLocationY, 170, 'd')
-			asteroidGroup.add(asteroid)
-			asteroidSpawned = true;
-			asteroid.image = asteroidModel;
-			asteroid.image.offset.x = 130;
-			noSmooth();
-		}
-		
-		asteroidDeleteParameter();
 		spaceshipCrashesAsteroid();
 		spawnCoin();
 		spaceshipTouchesCoin();
 		gameStateChanger();
-		
 
-		
 
-		
-		
+
+
+
+
 	}
-	else if(gameState == "lose") {
+	else if (gameState == "lose") {
 		background('#0e001b');
 		endScreenText();
 
-		
+
 	}
 }
 function preload() {
 	startText = loadImage('assets/start_image.png');
+	spaceshipModel = loadImage('assets/spaceshipImage.gif')
 	asteroidModel = loadImage('assets/asteroid.png');
 	coinModel = loadImage('assets/coin.png')
+
+
 
 }
 
 //**  This website helped https://processing.org/reference/mousePressed_.html**//
 function mousePressed() {
-	if(mousePressed) {
-		clickedCounter = clickedCounter+CLICKCOUNTERINCREASE;
+	if (mousePressed) {
+		clickedCounter = clickedCounter + CLICKCOUNTERINCREASE;
 	}
 }
 
@@ -154,36 +137,74 @@ function mousePressed() {
  // MOVEMENT OF THE spaceship
 *******************************************************/
 //** 
+
+function spaceshipCodeContainer() {
+	if (spaceshipSpawned == false) {
+		spaceship = new Sprite(200, 400, 200, 80, 'd');
+		spaceship.image = spaceshipModel;
+		spaceship.scale = 0.5;
+		spaceship.image.offset.y = 50;
+		spaceship.image.offset.x = -40;
+		spaceshipSpawned = true;
+	}
+	spaceship.rotationSpeed = 0;
+	spaceship.rotation = 0;
+	asteroidGroup.rotationSpeed = 0;
+	asteroidGroup.rotation = 0;
+	spaceshipMovement();
+}
+
+function asteroidCodeContainer() {
+	asteroidSpawningChance = random(1, 100);
+	asteroidSpawingLocationY = random(1, windowHeight);
+
+	if (asteroidSpawned == true) {
+		asteroidMovement();
+	}
+	if (ASTEROIDSPAWNINGCHANCEPARAMETER > asteroidSpawningChance) {
+		asteroid = new Sprite(windowWidth + 50, asteroidSpawingLocationY, 170, 'd')
+		asteroidGroup.add(asteroid)
+		asteroidSpawned = true;
+		asteroid.image = asteroidModel;
+		asteroid.image.offset.x = 130;
+		noSmooth();
+	}
+	asteroidDeleteParameter();
+}
+
+
+
+
 function spaceshipMovement() {
-if (kb.pressing('left')) {
-	spaceship.vel.x = -7;
-}
+	if (kb.pressing('left')) {
+		spaceship.vel.x = -7;
+	}
 
-else if (kb.pressing ('right')) {
-	spaceship.vel.x = 7;  
-}
+	else if (kb.pressing('right')) {
+		spaceship.vel.x = 7;
+	}
 
-if (kb.released('left')) {
-	spaceship.vel.x = 0;
-}
+	if (kb.released('left')) {
+		spaceship.vel.x = 0;
+	}
 
-else if (kb.released('right')) {
-	spaceship.vel.x = 0;
-}
-if (kb.pressing('up')) {
-	spaceship.vel.y = -7;
+	else if (kb.released('right')) {
+		spaceship.vel.x = 0;
+	}
+	if (kb.pressing('up')) {
+		spaceship.vel.y = -7;
 
-}
-else if(kb.pressing('down')) {
-	spaceship.vel.y = 7;
-}
-if (kb.released('up')) {
-	spaceship.vel.y = 0;
-}
+	}
+	else if (kb.pressing('down')) {
+		spaceship.vel.y = 7;
+	}
+	if (kb.released('up')) {
+		spaceship.vel.y = 0;
+	}
 
-else if (kb.released('down')) {
-	spaceship.vel.y = 0;
-}
+	else if (kb.released('down')) {
+		spaceship.vel.y = 0;
+	}
 }
 function asteroidMovement() {
 	asteroidGroup.vel.x = ASTEROIDSPEED;
@@ -194,35 +215,35 @@ function asteroidMovement() {
 /**MUST REMEMBER TO CHANGE BACK TO NORMAL**/
 function borders() {
 	// north wall 
-	north = new Sprite(windowWidth/2, 0, windowWidth, 3, 's' )
+	north = new Sprite(windowWidth / 2, 0, windowWidth, 3, 's')
 	north.color = 'red'
 
 	// south wall
-	south = new Sprite(windowWidth/2, windowHeight, windowWidth, 3, 's' )
+	south = new Sprite(windowWidth / 2, windowHeight, windowWidth, 3, 's')
 	south.color = 'red'
-	
+
 	// west wall
-	west = new Sprite(-400, windowHeight/2, 3, windowHeight, 's' )
+	west = new Sprite(-400, windowHeight / 2, 3, windowHeight, 's')
 	west.color = 'red'
 }
 
 
 // Checking if the asteroid has collided with the west wall
 function asteroidDeleteParameter() {
-	asteroidGroup.collides(west,asteroidDelete);
+	asteroidGroup.collides(west, asteroidDelete);
 }
 //
-function asteroidDelete(_west,_asteroid) {
+function asteroidDelete(_west, _asteroid) {
 	_asteroid.remove();
 	console.log("contact made")
 }
 
 // Checking if the spaceship has crashed into the asteroid
 function spaceshipCrashesAsteroid() {
-	asteroidGroup.collides(spaceship,spaceshipHit);
+	asteroidGroup.collides(spaceship, spaceshipHit);
 }
 // spaceship has crashed into an asteroid and the asteroid gets removed
-function spaceshipHit(_asteroid,_spaceship) {
+function spaceshipHit(_asteroid, _spaceship) {
 	_asteroid.remove();
 	console.log("contact made");
 	lives--
@@ -230,10 +251,10 @@ function spaceshipHit(_asteroid,_spaceship) {
 	crashParticles(praticlesSpawningPart);
 }
 function spaceshipTouchesCoin() {
-	coinGroup.collides(spaceship,spaceshipCollectCoin);
+	coinGroup.collides(spaceship, spaceshipCollectCoin);
 }
 // spaceship has crashed into an asteroid and the asteroid gets removed
-function spaceshipCollectCoin(_coin,_spaceship) {
+function spaceshipCollectCoin(_coin, _spaceship) {
 	_coin.remove();
 	console.log("coin collected");
 	score++
@@ -247,18 +268,18 @@ function spaceshipCollectCoin(_coin,_spaceship) {
 // chatgpt used to find the error
 /*****************************************************/
 function spawnCoin() {
-	coinSpawningChance = random(1,100);
-	coinSpawingLocationY = random(1,windowHeight);
-	if(COINSPAWNINGCHANCEPARAMETER>coinSpawningChance) {
-		coin = new Sprite(windowWidth+20, coinSpawingLocationY, 20, 'k' )
+	coinSpawningChance = random(1, 100);
+	coinSpawingLocationY = random(1, windowHeight);
+	if (COINSPAWNINGCHANCEPARAMETER > coinSpawningChance) {
+		coin = new Sprite(windowWidth + 20, coinSpawingLocationY, 20, 'k')
 		coin.image = coinModel;
 		noSmooth();
 		console.log("coin spawned");
 		coinGroup.add(coin);
 		coinMovement();
-		
+
 	}
-	
+
 }
 
 /*****************************************************/
@@ -282,10 +303,10 @@ function coinMovement() {
 /*****************************************************/
 function info() {
 	fill("#cbc83c");
-		playtext1 = "Lives " + lives;
-		playtext2 = "Score " + score;
-		text(playtext1, 50, 50);
-		text(playtext2, 50, 100);
+	playtext1 = "Lives " + lives;
+	playtext2 = "Score " + score;
+	text(playtext1, 50, 50);
+	text(playtext2, 50, 100);
 }
 
 
@@ -300,57 +321,57 @@ function info() {
 /*****************************************************/
 
 function gameStateChanger() {
-	if(gameStateChanged<1) {
-		if(lives <= 0) {
+	if (gameStateChanged < 1) {
+		if (lives <= 0) {
 			gameState = "lose";
-			gameStateChanged =1;
+			gameStateChanged = 1;
 			spaceship.remove();
 			asteroidGroup.remove();
 			coinGroup.remove();
-			}
+		}
 	}
 }
 
 function crashParticles(_particles) {
-	particlesSpawningAmount = _particles*5
-	for(i=0; i<particlesSpawningAmount; i++) {
+	particlesSpawningAmount = _particles * 5
+	for (i = 0; i < particlesSpawningAmount; i++) {
 		particlesGroup = new Group();
 		particles = new Sprite(spaceship.x, spaceship.y, 10, 10, 'n');
 		particles.color = "red";
-		particleMovementX = random(-5,5);
-		particleMovementY = random(-5,5);
+		particleMovementX = random(-5, 5);
+		particleMovementY = random(-5, 5);
 		particles.vel.x = particleMovementX;
 		particles.vel.y = particleMovementY;
 		particles.life = 30;
 		particlesGroup.add(particles);
-		
+
 	}
 }
 function createEndBackground() {
-	endBackground = new Sprite(0,0,windowWidth,windowHeight, 's');
+	endBackground = new Sprite(0, 0, windowWidth, windowHeight, 's');
 }
 
 function determiner() {
-	if(lives == 1) {
+	if (lives == 1) {
 		praticlesSpawningPart = lives + 4;
-	} else if(lives == 2) {
+	} else if (lives == 2) {
 		praticlesSpawningPart = lives + 2;
-	} else if(lives == 3) {
+	} else if (lives == 3) {
 		praticlesSpawningPart = lives;
-	} else if(lives == 4) {
-		praticlesSpawningPart = lives -2;
-	} else if(lives == 5) {
-		praticlesSpawningPart = lives -4;
+	} else if (lives == 4) {
+		praticlesSpawningPart = lives - 2;
+	} else if (lives == 5) {
+		praticlesSpawningPart = lives - 4;
 	}
 }
 
 function endScreenText() {
 	fill("#cbc83c");
 	textSize(30)
-	text("YOUR SHIP HAS BEEN DESTROYED", windowWidth/2-300, 400);
-	text("SCORE " + score, windowWidth/2-300, 440);
+	text("YOUR SHIP HAS BEEN DESTROYED", windowWidth / 2 - 300, 400);
+	text("SCORE " + score, windowWidth / 2 - 300, 440);
 	textSize(10)
-	text("refresh the page to try again", windowWidth/2-250, 800);
+	text("refresh the page to try again", windowWidth / 2 - 250, 800);
 	textAlign(centre)
 
 }
